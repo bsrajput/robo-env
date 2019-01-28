112ddbe640d56588a66a10b8f97300c156633981
abhishekamralkar <abhishekanand1984@gmail.com>
2019-01-28 12:29:56 -0500

    update repo list to add non free software repo

---
+++ setup_repo.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# run this script as root
+
+
+SOURCE_FILE=/etc/apt/sources.list
+
+update_repo() {
+mv $SOURCE_FILE "$SOURCE_FILE.bk"
+sudo cat >>$SOURCE_FILE <<EOF
+deb http://security.debian.org/debian-security stretch/updates main
+deb-src http://security.debian.org/debian-security stretch/updates main
+
+deb http://deb.debian.org/debian stretch main
+deb-src http://deb.debian.org/debian stretch main
+
+deb http://deb.debian.org/debian stretch-updates main
+deb-src http://deb.debian.org/debian stretch-updates main
+
+# Debian 9 "Stretch"
+deb http://httpredir.debian.org/debian/ stretch main contrib non-free
+EOF
+}
+
+main {
+  update_repo
+  }
