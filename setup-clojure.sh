94ff5b1633f44cc93bc1dddefe890a6be04c954e
abhishekamralkar <abhishekanand1984@gmail.com>
2019-02-03 19:49:57 +0530

    adding the hyperlinks for the softwares

--- setup-clojure.sh
+++ setup-clojure.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+# install clojure and lein
+curl -O https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+sudo chmod 755 lein && sudo mv lein /usr/bin
 curl -O https://download.clojure.org/install/linux-install-1.10.0.411.sh
 chmod +x linux-install-1.10.0.411.sh
 sudo ./linux-install-1.10.0.411.sh
